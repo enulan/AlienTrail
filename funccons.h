0b9dd85eee1bbf9fc34ea7fb2972a8318d76ef45
elnulan <elnulan@wichita.edu>
2013-04-05 09:48:28 -0500

    commit from hk

---
+++ funccons.h
@@ -0,0 +1,19 @@
+
+//header file containing fundamental constants necessary to Alien Trail
+
+#ifndef ATFUNCONST
+#define ATFUNCONST
+
+#include <cmath>
+
+using namespace std;
+
+float EARTH_MASS = 5.98 * pow(10, 24);
+float MOON_MASS = 7.35 * pow(10, 22);
+float SUN_MASS = 1.99 * pow(10, 30);
+short EARTH_RADIUS = 6.38 * pow(10, 3);
+short MOON_RADIUS = 1.74 * pow(10, 3);
+short SUN_RADIUS = 6.96 * pow(10, 5);
+
+#endif
+
